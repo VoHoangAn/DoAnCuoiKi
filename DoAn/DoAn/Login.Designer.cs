5248218226659c1a7c4fe2210d2846477f017cdf
bachga123 <63158723+bachga123@users.noreply.github.com>
2020-04-07 17:03:47 +0700

    Update Login.Designer.cs

--- DoAn/DoAn/Login.Designer.cs
+++ DoAn/DoAn/Login.Designer.cs
@@ -71,6 +71,7 @@
             // 
             this.passwordTextBox.Location = new System.Drawing.Point(152, 98);
             this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.PasswordChar = '*';
             this.passwordTextBox.Size = new System.Drawing.Size(235, 22);
             this.passwordTextBox.TabIndex = 3;
             // 
